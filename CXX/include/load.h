5e5c3ae119144fcc141b83300abe6f77e04ce54c
TwilCynder <twilcynder@gmail.com>
2021-04-10 20:58:42 +0200

    renamed c++ directory

--- CXX/include/load.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "DataFile.h"
-
-namespace Load{
-    bool loadGameFile(const char*, GameData&);
-}
--- CXX/src/Frame.cpp
+++
@@ -1,9 +0,0 @@
-#include "Frame.h"
-#include "Debug.h"
-
-Frame::Frame() : 
-    duration(0),
-    movement({0, 0}),
-    movement_type({0, 0})
-{
-}
