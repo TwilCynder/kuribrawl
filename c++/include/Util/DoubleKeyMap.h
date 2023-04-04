20afe6e80b6cff59994ae686140bdeb63f1b4eb1
TwilCynder <twilcynder@gmail.com>
2023-04-04 20:59:47 +0200

    DoubleKeyMap

--- c++/include/Util/DoubleKeyMap.h
+++ c++/include/Util/DoubleKeyMap.h
@@ -3,8 +3,22 @@
 #include "structures.h"
 
 namespace Kuribrawl {
-    template<typename K1, typename K2, typename V, typename _Key = std::pair<K1, K2>, template <typename Tr1, typename Tr2> typename _Refpair = Kuribrawl::refpair>
-    class DoubleKeyMap : public std::map<_Key, V, std::less<>>{
+    template <typename P, typename T1, typename T2>
+    concept PairOf = std::is_base_of<std::pair<T1, T2>, P>::value;
+
+    template <template <typename T1, typename T2> typename P, typename T1, typename T2>
+    concept RefPairOf = requires (P<T1, T2> p) {
+        {p.first} -> std::same_as<T1&>;
+        {p.second} -> std::same_as<T2&>;
+    };
+
+    template <typename K1, typename K2, typename V, typename _Key, template <typename T1, typename T2> typename _Refpair>
+    concept ValidDoubleKeyMapTypeArguments = 
+        PairOf<_Key, K1, K2> && RefPairOf<_Refpair, K1, K2>;
+
+    template<typename K1, typename K2, typename V, typename _Key, template <typename Tr1, typename Tr2> typename _Refpair>
+        requires ValidDoubleKeyMapTypeArguments<K1, K2, V, _Key, _Refpair>
+    class __DoubleKeyMap : public std::map<_Key, V, std::less<>>{
         using base_type = std::map<_Key, V, std::less<>>;
 
         public:
@@ -12,12 +26,6 @@ namespace Kuribrawl {
         using ref_key = _Refpair<const K1, const K2>;
         //using const_ref_key = refbipair<const K>;
 
-        _Key make_key(const K1& k1, const K2& k2){
-            return _Key(k1, k2);
-        }
-        _Key make_key(K1&& k1, K2&& k2){
-            return _Key(k1, k2);
-        }
         /*
         const_ref_key make_key_ref(const K& k1, const K& k2){
             std::cout << "\nmake_key_ref\n";
@@ -29,10 +37,6 @@ namespace Kuribrawl {
 
         template<class... Args>
         void try_emplace(const K1& k1, const K2& k2, Args&&... args){
-            base_type::try_emplace(_Key(k1, k2), args...);
-        }
-        template<class... Args>
-        void try_emplace(K1&& k1, K2&& k2, Args&&... args){
             base_type::try_emplace(_Key(k1, k2), args...);
         }
 
@@ -53,7 +57,7 @@ namespace Kuribrawl {
             auto it = base_type::find(make_key_ref(k1, k2));
             if (it != base_type::end()){
                 return it->second;
-            } 
+            }
             return V{};
         }
 
@@ -65,7 +69,50 @@ namespace Kuribrawl {
             } 
             return V{};
         }
+
+        struct tuple_iterator : public base_type::iterator{
+            using base_it = base_type::iterator;
+            auto operator*(){
+                const _Key& k = (*this)->first;
+                const V& v = (*this)->second;
+                return std::tuple<const K1&, const K2&, const V&>(k.first, k.second, v);
+            }
+        };
+
+        tuple_iterator tbegin(){
+            return (tuple_iterator)base_type::begin();
+        }
+
+        struct DoubleKeyRange;
+
+        DoubleKeyRange& trange() {
+            return (DoubleKeyRange&)(*this);
+        }
     };
+
+    template <typename K1, typename K2, typename V, typename _Key, template <typename T1, typename T2> typename _Refpair> 
+    struct __DoubleKeyMap<K1, K2, V, _Key, _Refpair>::DoubleKeyRange : public __DoubleKeyMap<K1, K2, V, _Key, _Refpair>{
+        auto begin(){
+            return tbegin();
+        }
+    };
+
+    template<typename K1, typename K2, typename V, typename _Key = std::pair<K1, K2>, template <typename Tr1, typename Tr2> typename _Refpair = refpair_std>
+    class DoubleKeyMap;
+
+    template<typename K1, typename K2, typename V, typename _Key, template <typename Tr1, typename Tr2> typename _Refpair>
+        requires(!(std::is_reference_v<K1> && std::is_reference_v<K2>))
+    class DoubleKeyMap<K1, K2, V, _Key, _Refpair> : public __DoubleKeyMap<K1, K2, V, _Key, _Refpair> {
+        template<class... Args>
+        void try_emplace(K1&& k1, K2&& k2, Args&&... args){
+            try_emplace(_Key(k1, k2), args...);
+        }
+    };
+
+    template<typename K1, typename K2, typename V, typename _Key, template <typename Tr1, typename Tr2> typename _Refpair>
+        requires(std::is_reference_v<K1> || std::is_reference_v<K2>)
+    class DoubleKeyMap<K1, K2, V, _Key, _Refpair> : public __DoubleKeyMap<K1, K2, V, _Key, _Refpair>
+    {};
 
     template <typename K, typename V>
     using DoubleKeyMapST = DoubleKeyMap<K, K, V>;
