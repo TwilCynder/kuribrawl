86374d241821471e3e93730ace930e01a8b3a9d1
TwilCynder <twilcynder@gmail.com>
2023-07-29 00:50:49 +0200

    tried to put a rectangle in the game constants

--- c++/include/GameConstants.h
+++ c++/include/GameConstants.h
@@ -1,5 +1,8 @@
+#include "Util/structures.h"
+
 constexpr struct {
-    double camera_max_speed = 10;
+    double camera_max_speed = 1;
+    Kuribrawl::Rect<double> camera_nomove_area = {0.5, 0.5, 0.5, 0.5};
 } gameConstants;
 
 constexpr double camera_max_speed_squared = gameConstants.camera_max_speed * gameConstants.camera_max_speed;
