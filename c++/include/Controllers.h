272e56ea5751a0a29fdb9713bd13eb9c18a6aaf9
TwilCynder <twilcynder@gmail.com>
2024-01-17 00:49:25 +0100

    Controller WIP

--- c++/include/Controllers.h
+++ c++/include/Controllers.h
@@ -18,19 +18,29 @@ class Controller {
 
     using ButtonsMapping = std::vector<SDL_GameControllerButton>;
 
+    static unique_ptr<Controller> opencontroller(int controller_id, ControllersData& cd);
+    static unique_ptr<Controller> openKeyboardController(ControllersData& cd);
+
+    void setControllerType(const ControllerType* c);
+
+    private:
+    Controller(int joystick_id);
+
     //These attributes determine the behavior of the controller or keyboard. They must be set (or unset, as long as everything is valid) everytime the controller changes
-    bool active; ///< True if the controller is active ; if false, the other attributes must not be used.
+    //bool active; ///< True if the controller is active ; if false, the other attributes must not be used.
     const ControllerType* controller_type;
-    SDL_GameController* controller; ///< MUST BE NULL IF NOT OPEN CURRENTLY
+    SDL_GameController* gamecontroller; ///< MUST BE NULL IF NOT OPEN CURRENTLY
 	SDL_Joystick* joystick;
     int joystick_id; ///< Numerical ID of the controller given by SDL, or -1 if using the keyboard.
     const ControllerLayout* current_controller_layout;    ///< The controller layout of the current controller, if any.
-    ButtonsMapping controller_buttons_mapping;
+    //ButtonsMapping controller_buttons_mapping;
 
     Port* port; //port this controller is plugged in. Null if unpluged controlled.  
 
     bool isButtonPressed(int id) const;
 
-    
+    bool init(int controller_id, ControllersData& cd);
+    bool initAsKeyboard(ControllersData& cd);
+    bool initButtonMapping();
 
 };
--- c++/src/Controllers.cpp
+++ c++/src/Controllers.cpp
@@ -1,10 +1,67 @@
 #include "Controllers.h"
+#include "ControllersData.h"
 #include "Port.h"
 
 #define MAPPING_NORMAL_SIZE 32
 #define isKeyboard (joystick_id == JOSTICKID_KEYBOARD)
 
-void ControllersManager::removeController(int id){
-    if (id > NB_CONTROLLERS) throw KBFatal("Attemp to remove controller with id above the limit");
-    controllers[id] = 0;
+
+bool Controller::init(int controller_id, ControllersData& cd){
+    gamecontroller = SDL_GameControllerOpen(controller_id);
+    joystick = SDL_GameControllerGetJoystick(gamecontroller);
+    SDL_JoystickID instance_id = SDL_JoystickInstanceID(joystick);
+
+    Debug::log("open controller");
+    Debug::log(gamecontroller);
+    Debug::log(joystick);
+
+    if (instance_id < 0){
+        Debug::out << "Error while trying to open controller " << controller_id << '\n';
+        Debug::log(SDL_GetError());
+        gamecontroller = nullptr;
+        joystick = nullptr;
+        return false;
+    }
+
+    joystick_id = instance_id;
+
+    //STORING THE BUTTON MAPPING
+    if (!initButtonMapping()){
+        Debug::out << "Error while trying to open controller : the SDL mapping is ill-formed.\n";
+        return false;
+    }
+
+    ControllerType* ct = cd.getControllerFromMapping(SDL_GameControllerMapping(gamecontroller));
+    if (!ct) ct = cd.getDefaultController();
+    setControllerType(ct);
+}
+
+
+unique_ptr<Controller> Controller::opencontroller(int controller_id, ControllersData& cd)
+{
+    unique_ptr<Controller> controller;
+    Debug::out << "Constructing controller from ID " << controller_id << '\n';
+    if (controller->init(controller_id, cd)) {
+        return controller;
+    } else {
+        return nullptr;
+    }
+}
+
+bool Controller::initAsKeyboard(ControllersData& cd){
+    joystick_id = JOSTICKID_KEYBOARD;
+    setControllerType(cd.getKeyboardController());
+
+    gamecontroller = nullptr;
+    joystick = nullptr;
+}
+
+unique_ptr<Controller> Controller::openKeyboardController(ControllersData& cd){
+    unique_ptr<Controller> controller;
+    Debug::out << "Constructing controller for keyboard\n";
+    if (controller->initAsKeyboard(cd)) {
+        return controller;
+    } else {
+        return nullptr;
+    }
 }
