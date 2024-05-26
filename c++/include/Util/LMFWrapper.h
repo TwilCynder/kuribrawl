f878182f2c46380a2c52cb8186f2d08acedf8668
TwilCynder <twilcynder@gmail.com>
2024-05-26 23:52:47 +0200

    ConstLogWrapper + helper macro instead of function

--- c++/include/Util/LMFWrapper.h
+++ c++/include/Util/LMFWrapper.h
@@ -2,7 +2,8 @@
 
 #include <ostream>
 
-template <class T, std::ostream&(T::*F)(std::ostream&)>
+
+template <class T, std::ostream&(T::*F)(std::ostream&) >
 class LogWrapper {
     T* obj;
 
@@ -15,7 +16,33 @@ class LogWrapper {
     }
 };
 
+/*
 template <class T>
-auto lmf_wrap(T* obj, std::ostream&(T::*F)(std::ostream&)){
+constexpr auto lmf_wrap(T* obj, std::ostream&(T::*F)(std::ostream&) ){
+    return LogWrapper<T, F>(obj);
+}*/
+
+
+template <class T, std::ostream&(T::*F)(std::ostream&) const>
+class ConstLogWrapper {
+    const T* obj;
+
+    public:
+
+    ConstLogWrapper(const T* obj_): obj(obj_){}
+
+    std::ostream& operator()(std::ostream& os) const {
+        return (obj->*F)(os);
+    }
+};
+
+/*
+template <class T>
+auto lmf_wrap(T* obj, std::ostream&(T::*F)(std::ostream&) const){
     return LogWrapper<T, F>(obj);
 }
+*/
+
+
+#define LMFWRAP(cl, method, obj) LogWrapper<cl, method>(obj)
+#define CLMFWRAP(cl, method, obj) ConstLogWrapper<cl, method>(obj)
